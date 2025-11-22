56dfdfd63b67d1f4dbc48d9d1271f71a132954bc
jhonarkks <jhonarkks@domimarket.com>
2025-11-22 03:31:01 +0330

    Landing renovada estilo ETVX Store

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Login con Discord
+// Maneja el click del login Discord
 document.querySelectorAll(".login-btn, .login-btn-header").forEach(btn => {
-    btn.addEventListener("click", () => {
-        window.location.href = "https://domimarket-login.vercel.app/api/auth/discord";
-    });
+  btn.addEventListener("click", () => {
+    window.location.href = "https://domimarket-login.vercel.app/api/auth/discord";
+  });
 });
